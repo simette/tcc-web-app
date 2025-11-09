ea8b2477ae4a21182f69cb7de254d2a891233182
gabriely <gabrielysimette@alunos.utfpr.edu.br>
2025-11-09 14:26:54 -0500

    Format response

--- backend/chat_connector.py
+++ backend/chat_connector.py
@@ -9,6 +9,14 @@ SERVING_ENDPOINT  = os.getenv('SERVING_ENDPOINT')
 DATABRICKS_HOST = os.getenv('DATABRICKS_HOST')
 DATABRICKS_TOKEN  = os.getenv('DATABRICKS_TOKEN')
 
+
+def format_response(response_content):
+    try:
+        return response_content['choices'][0]['message']['content']
+    except Exception as e:
+        return 'Desculpe, houve um erro. Tente novamente.'
+
+
 def send_question(system_message, user_message) -> str:
 
     workspace_url = DATABRICKS_HOST
@@ -37,7 +45,8 @@ def send_question(system_message, user_message) -> str:
         "max_tokens": max_tokens,
         "temperature": 0.7
     }
+
     response = requests.post(url, headers=headers, json=payload, timeout=30)
-    result = response.json()
+    result = format_response(response.json())
 
     return result
